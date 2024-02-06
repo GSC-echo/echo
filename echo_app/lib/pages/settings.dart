d8eddf97cc9dcf2a7c0d277c7d8a0305f5320c63
Chaeyoung Shin <scy0723123@gmail.com>
2024-02-06 18:48:29 +0900

    settings+ios

--- echo_app/lib/pages/settings.dart
+++ echo_app/lib/pages/settings.dart
@@ -1,5 +1,6 @@
-
+import 'package:echo_app/config/colors.dart';
 import 'package:echo_app/main.dart';
+import 'package:echo_app/pages/login.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:google_sign_in/google_sign_in.dart';
@@ -16,50 +17,191 @@ class _SettingsState extends State<Settings> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: SafeArea(
-      child: Column(crossAxisAlignment: CrossAxisAlignment.center, 
-      children: [
-        Expanded(
-            child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
+      body: SafeArea(
+        child: Column(
           children: [
-            InkWell(
-                onTap: () async {
+            Image.asset('lib/config/images/echo.png', width: 150, height: 150),
+            Align(
+              alignment: Alignment.topCenter,
+              child: Container(
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(70),
+                  color: Colors.white,
+                  border: Border.all(
+                      color: BackgroundColor.mainGreen.withOpacity(0.70),
+                      width: 3),
+                ),
+                height: 350,
+                width: 350,
+                child: Container(
+                  //margin: const EdgeInsets.only(top: 25.0),
+                  child: Column(
+                    children: [
+      //Record
+                      Container(
+                        width: 300,
+                        height: 75,
+                        child: InkWell(
+                          onTap: () {
+                            signInWithGoogle(context);
+                          },
+                          child: Card(
+                            color: BackgroundColor.mainGreen.withOpacity(0.75),
+                            margin: const EdgeInsets.fromLTRB(30, 30, 30, 0),
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(15),
+                            ),
+                            elevation: 3,
+                            child: Center(
+                              // 중앙 정렬을 위한 Center 위젯 추가
+                              child: Text(
+                                "RECORD",
+                                textAlign: TextAlign.center, // 텍스트 가운데 정렬
+                                style: TextStyle(
+                                  color: Colors.white,
+                                  fontFamily: 'Jua',
+                                  fontSize: 22,
+                                ),
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
+                      Container(
+                        width: 300,
+                        height: 75,
+                        child: InkWell(
+                          onTap: () {
+                            signInWithGoogle(context);
+                          },
+                          child: Card(
+                            color: BackgroundColor.mainGreen.withOpacity(0.75),
+                            margin: const EdgeInsets.fromLTRB(30, 30, 30, 0),
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(15),
+                            ),
+                            elevation: 3,
+                            child: Center(
+                              // 중앙 정렬을 위한 Center 위젯 추가
+                              child: Text(
+                                "SIGN OUT",
+                                textAlign: TextAlign.center, // 텍스트 가운데 정렬
+                                style: TextStyle(
+                                  color: Colors.white,
+                                  fontFamily: 'Jua',
+                                  fontSize: 22,
+                                ),
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
+                      Container(
+                        width: 300,
+                        height: 75,
+                        child: InkWell(
+                          onTap: () {
+                            signInWithGoogle(context);
+                          },
+                          child: Card(
+                            color: BackgroundColor.mainGreen.withOpacity(0.75),
+                            margin: const EdgeInsets.fromLTRB(30, 30, 30, 0),
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(15),
+                            ),
+                            elevation: 3,
+                            child: Center(
+                              // 중앙 정렬을 위한 Center 위젯 추가
+                              child: Text(
+                                "Privacy & Security",
+                                textAlign: TextAlign.center, // 텍스트 가운데 정렬
+                                style: TextStyle(
+                                  color: Colors.white,
+                                  fontFamily: 'Jua',
+                                  fontSize: 22,
+                                ),
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
+
+                      Container(
+                        width: 300,
+                        height: 75,
+                        child: InkWell(
+                          onTap: () {
+                            signInWithGoogle(context);
+                          },
+                          child: Card(
+                            color: BackgroundColor.mainGreen.withOpacity(0.75),
+                            margin: const EdgeInsets.fromLTRB(30, 30, 30, 0),
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(15),
+                            ),
+                            elevation: 3,
+                            child: Center(
+                              // 중앙 정렬을 위한 Center 위젯 추가
+                              child: Text(
+                                "Help & Support",
+                                textAlign: TextAlign.center, // 텍스트 가운데 정렬
+                                style: TextStyle(
+                                  color: Colors.white,
+                                  fontFamily: 'Jua',
+                                  fontSize: 22,
+                                ),
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
+
+                    ],
+                  ),
+                ),
+              ),
+            ),
+            Container(
+              width: 250,
+              height: 75,
+              child: InkWell(
+                onTap: () async{
                   GoogleSignIn().signOut();
                   FirebaseAuth.instance.signOut();
                   Navigator.of(context as BuildContext).pushReplacement(MaterialPageRoute(
-                    builder: (context) => Splash(),
-      ));
+                    builder: (context) => const Splash(),
+                  ));
                 },
                 child: Card(
+                  color: Colors.white,
                   margin: const EdgeInsets.fromLTRB(30, 30, 30, 0),
                   shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(7)),
+                    borderRadius: BorderRadius.circular(15),
+                    side: BorderSide(color: const Color.fromARGB(255, 237, 104, 94), width:3)
+                  ),
                   elevation: 3,
                   child: Row(
                       mainAxisAlignment: MainAxisAlignment.center,
                       children: [
                         Icon(
                                     Icons.logout,
-                                    size: 40,
-                                    color: Colors.black,
+                                    size: 20,
+                                    color: Colors.red,
                                   ),
                         const SizedBox(
                           width: 10,
                         ),
                         const Text("Log out", 
                             style: TextStyle(
-                                color: Color.fromARGB(255, 13, 8, 8),
-                                fontFamily: 'Roboto',
-                                fontSize: 22,
-                                fontWeight: FontWeight.bold))
+                                color:const Color.fromARGB(255, 237, 104, 94),
+                                fontFamily: 'Jua',
+                                fontSize: 18))
                       ]),
                 ))
+            ),
           ],
-        ))
-      ]),
-    ));
+        ),
+      ),
+    );
   }
-
-
 }
